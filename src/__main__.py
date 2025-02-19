43d411312e75d64bb6d3cbdfb1619c9a7caac40a
marioUCM <mariogra@ucm.es>
2025-02-19 16:44:00 +0100

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -10,19 +10,37 @@ def ejecutar_script(script):
     else:
         print(f"Error: No se encontró el archivo '{script}'")
 
+
 def ejecutar():
     """Ejecuta todos los archivos .py en la ruta especificada."""
-    
-    print('..')  
+
     # Obtiene la ruta absoluta del directorio donde está este script
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Construye la ruta del script dinámicamente
+    #Extraccion
+
     script = os.path.join(base_dir, "extraccion", "scraper_peleadores_ufc.py")
-    
     ejecutar_script(script)
 
+    script = os.path.join(base_dir, "extraccion", "WebScrapCombates.py")
+    ejecutar_script(script)
+
+    script = os.path.join(base_dir, "extraccion", "scraper_tapology_bf.py")
+    ejecutar_script(script)
+
+    #Transformacion
+    script = os.path.join(base_dir, "transformacion", "tratamiento_peleas.py")
+
+    ejecutar_script(script)
+    script = os.path.join(base_dir, "transformacion", "tratamiento_peleadores.py")
+    
+    #Analisis
+    ejecutar_script(script)
+    script = os.path.join(base_dir, "analisis", "ranking_v2.py")
+    
+    #Prediccion...
+
+
 if __name__ == "__main__":
-    print('.')
     ejecutar()
 
